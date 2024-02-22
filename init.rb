480f980609f52dbae50d346c3a4dc362e70c2b01
petitglacon <estebangagneur03@gmail.com>
2024-02-22 23:47:43 +0100

    working like a charm

--- init.rb
+++ init.rb
@@ -5,4 +5,16 @@ Redmine::Plugin.register :mm_hooks do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+
+  settings :default => {
+    'mm_url' => 'https://mm.cameros.fr',
+    'mm_key' => '',
+    'rm_url' => 'https://projects.cameros.fr',
+    'obs_user' => '',
+    'obs_priorities' => [],
+    'obs_trackers' => [],
+    'obs_statuses' => []
+  },
+  :partial => 'settings/mm_hooks_settings'
 end
--- lib/listener.rb
+++ lib/listener.rb
@@ -3,9 +3,45 @@ require 'httparty'
 class Listener < Redmine::Hook::ViewListener
     
   include HTTParty
+
+  # structure ticket
+  # "is_private"=>"0", "tracker_id"=>"1", "subject"=>"zzzzzzzzzz", "description"=>"dzqdqz", "status_id"=>"1", "priority_id"=>"2", "assigned_to_id"=>"", "parent_issue_id"=>"", "start_date"=>"2024-02-22", "due_date"=>"", "estimated_hours"=>"", "done_ratio"=>"0", "watcher_user_ids"=>[""]
   
   def controller_issues_new_before_save(context = {})
-    context[:issue].watcher_user_ids = [5] # TODO mettre l'id de l'équipe technique
+    ticket = context[:issue]
+    @shouldBeWatched = false
+
+    # 
+    if (
+      Setting.plugin_mm_hooks['obs_priorities'] != nil &&
+      Setting.plugin_mm_hooks['obs_priorities'].index(ticket.priority_id.to_s) != nil
+    ) 
+      then @shouldBeWatched = true
+      else @shouldBeWatched = false
+    end
+
+    if (
+      Setting.plugin_mm_hooks['obs_trackers'] != nil &&
+      Setting.plugin_mm_hooks['obs_trackers'].index(ticket.tracker_id.to_s) != nil
+    ) 
+      then @shouldBeWatched = true
+      else @shouldBeWatched = false
+    end
+
+    if (
+      Setting.plugin_mm_hooks['obs_statuses'] != nil &&
+      Setting.plugin_mm_hooks['obs_statuses'].index(ticket.status_id.to_s) != nil
+    ) 
+      then @shouldBeWatched = true
+      else @shouldBeWatched = false
+    end
+
+    if (@shouldBeWatched) then
+        ## set équipe technique en obs
+        watchers = ticket.watcher_user_ids
+        watchers.push(Setting.plugin_mm_hooks['obs_user'].to_i)
+        ticket.watcher_user_ids = watchers
+    end
   end
 
   def controller_issues_new_after_save(context = {})
